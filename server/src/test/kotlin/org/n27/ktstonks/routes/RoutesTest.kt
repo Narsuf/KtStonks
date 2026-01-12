93c745869d0524298dac10aa47729e694ab6a881
Narsuf <jorgedguezm@gmail.com>
2026-01-12 21:19:09 +0100

    Valuation tested

--- server/src/test/kotlin/org/n27/ktstonks/routes/RoutesTest.kt
+++ server/src/test/kotlin/org/n27/ktstonks/routes/RoutesTest.kt
@@ -1,5 +1,6 @@
 package org.n27.ktstonks.routes
 
+import io.ktor.client.HttpClient
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
@@ -8,6 +9,7 @@ import io.ktor.server.application.install
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyDouble
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -23,15 +25,8 @@ class RoutesTest {
     private val useCase: UseCase = mock()
 
     @Test
-    fun `test get stock by symbol success`() = testApplication {
-        application {
-            install(ServerContentNegotiation) { json() }
-            routing { stockRoutes(useCase) }
-        }
+    fun `test get stock by symbol success`() = testWithApplication { client ->
         whenever(useCase.getStock(anyString())) doReturn Result.success(getStock("AAPL"))
-        val client = createClient {
-            install(ClientContentNegotiation) { json() }
-        }
 
         val response = client.get("/stock/AAPL")
 
@@ -40,16 +35,44 @@ class RoutesTest {
     }
 
     @Test
-    fun `test get stock by symbol error`() = testApplication {
-        application {
-            install(ServerContentNegotiation) { json() }
-            routing { stockRoutes(useCase) }
-        }
+    fun `test get stock by symbol error`() = testWithApplication { client ->
         whenever(useCase.getStock("AAPL")) doReturn Result.failure(Exception())
 
         val response = client.get("/stock/AAPL")
 
         assertEquals(HttpStatusCode.InternalServerError, response.status)
+    }
+
+    @Test
+    fun `test post valuation success`() = testWithApplication { client ->
+        whenever(useCase.addCustomValuation(anyString(), anyDouble(), anyDouble())) doReturn Result.success(Unit)
+
+        val response = client.post("/stock/AAPL/valuation?valuationFloor=1.0&epsGrowth=1.0")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Valuation updated", response.bodyAsText())
+    }
+
+    @Test
+    fun `test post valuation error`() = testWithApplication { client ->
+        whenever(useCase.addCustomValuation(anyString(), anyDouble(), anyDouble())) doReturn Result.failure(Exception())
+
+        val response = client.post("/stock/AAPL/valuation?valuationFloor=1.0&epsGrowth=1.0")
+
+        assertEquals(HttpStatusCode.InternalServerError, response.status)
+    }
+
+    private fun testWithApplication(
+        block: suspend ApplicationTestBuilder.(client: HttpClient) -> Unit
+    ) = testApplication {
+        application {
+            install(ServerContentNegotiation) { json() }
+            routing { stockRoutes(useCase) }
+        }
+        val client = createClient {
+            install(ClientContentNegotiation) { json() }
+        }
+        block(client)
     }
 
     private fun getJson(path: String): String = this::class.java.classLoader.getResource(path)!!.readText().removeSuffix("\n")
