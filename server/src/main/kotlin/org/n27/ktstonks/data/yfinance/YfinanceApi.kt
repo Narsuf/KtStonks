0f09d863fcd5c75688352359a180c6bf312b9d3e
Jorge <jorge.dominguez@n26.com>
2026-05-06 21:12:43 +0200

    Remove constants

--- server/src/main/kotlin/org/n27/ktstonks/data/yfinance/YfinanceApi.kt
+++ server/src/main/kotlin/org/n27/ktstonks/data/yfinance/YfinanceApi.kt
@@ -6,20 +6,17 @@ import io.ktor.client.request.*
 import org.n27.ktstonks.data.yfinance.model.StockRaw
 import org.slf4j.LoggerFactory
 
-private const val STOCK_URL = "/stock"
-private const val STOCKS_URL = "/stocks"
-
 class YfinanceApi(private val url: String, private val httpClient: HttpClient) {
 
     private val logger = LoggerFactory.getLogger(YfinanceApi::class.java)
 
     suspend fun getStock(symbol: String): StockRaw {
         logger.info("getStock request triggered")
-        return httpClient.get("$url$STOCK_URL/$symbol").body()
+        return httpClient.get("$url/stock/$symbol").body()
     }
 
     suspend fun getStocks(symbols: String): List<StockRaw> {
         logger.info("getStocks request triggered")
-        return httpClient.get("$url$STOCKS_URL") { parameter("symbols", symbols) }.body()
+        return httpClient.get("$url/stocks") { parameter("symbols", symbols) }.body()
     }
 }
